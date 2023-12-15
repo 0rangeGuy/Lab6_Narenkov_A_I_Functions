dd75b5e210a00e884ae1009ae9fa08a5553a6eed
0rangeGuy <148427436+0rangeGuy@users.noreply.github.com>
2023-12-16 00:32:21 +0700

    Delete 5th_task.js

--- 5th_task.js
+++
@@ -1,8 +0,0 @@
-alert("Введите два числа, для нахождения их остатка от деления.");
-DigitOne = prompt('Первое число.');
-DigitTwo = prompt('Второе число число.');
-DigitOne = Number(DigitOne);
-DigitTwo = Number(DigitTwo);
-Result = DigitOne % DigitTwo;
-alert('Результат: '+DigitOne+' % '+ DigitTwo+' = '+ Result);
-console.log('Результат: ' + DigitOne + ' % ' + DigitTwo + ' = ' + Result);
